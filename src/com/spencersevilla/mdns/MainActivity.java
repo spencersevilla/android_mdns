53efe1e3d7a6ac4fd5973f21719a49bfa97bb9f2
Spencer Sevilla <spencersevilla@gmail.com>
2012-07-31 15:01:57 -0700

    setup
    Changed test example over to a list, removed build and gen directories,
    included android build instructions

---
+++ src/com/spencersevilla/mdns/MainActivity.java
@@ -0,0 +1,42 @@
+package com.spencersevilla.mdns;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+ 
+public class MainActivity extends ListActivity {
+ 
+	static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+			"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
+ 
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+ 
+		// no more this
+		// setContentView(R.layout.list_fruit);
+ 
+		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main,FRUITS));
+ 
+		ListView listView = getListView();
+		listView.setTextFilterEnabled(true);
+ 
+		listView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+			    // When clicked, show a toast with the TextView text
+			    Toast.makeText(getApplicationContext(),
+				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+ 
+	}
+ 
+}
