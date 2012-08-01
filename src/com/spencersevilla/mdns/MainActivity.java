bc0a7d14b65e61de441c003b4f70a7c1627d7332
Spencer Sevilla <spencersevilla@gmail.com>
2012-08-01 14:40:57 -0700

    implemented list layout and buttons
    UI all works roughly as intended, about to start hooking up the actual
    mans code now!

--- src/com/spencersevilla/mdns/MainActivity.java
+++ src/com/spencersevilla/mdns/MainActivity.java
@@ -1,6 +1,6 @@
 package com.spencersevilla.mdns;
 
-import android.app.ListActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,34 +9,98 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.CheckedTextView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends Activity {
  
-public class MainActivity extends ListActivity {
- 
-	static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
-			"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
-			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
- 
+	ListView groupListView;
+	ListView serviceListView;
+	
+	ArrayList<String> groupList;
+	ArrayAdapter<String> groupAdapter;
+	
+	ArrayList<String> serviceList;
+	
+ 	TabHost tabHost;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
- 
-		// no more this
-		// setContentView(R.layout.list_fruit);
- 
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main,FRUITS));
- 
-		ListView listView = getListView();
-		listView.setTextFilterEnabled(true);
- 
-		listView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-			    // When clicked, show a toast with the TextView text
-			    Toast.makeText(getApplicationContext(),
-				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+
+		// Initialize ContentView and tabs
+	    setContentView(R.layout.activity_main);
+
+		tabHost = (TabHost) findViewById(R.id.tabHost);
+		tabHost.setup();
+
+		TabSpec spec1 = tabHost.newTabSpec("tab1");
+		spec1.setContent(R.id.tab1);
+		spec1.setIndicator("DNS Groups");
+		tabHost.addTab(spec1);
+
+		TabSpec spec2 = tabHost.newTabSpec("tab2");
+		spec2.setContent(R.id.tab2);
+		spec2.setIndicator("Services Advertised");
+		tabHost.addTab(spec2);
+		
+		initializeGroupList();
+		initializeServiceList();
+	}
+	
+	private void initializeGroupList() {
+		groupList = new ArrayList<String>(Arrays.asList(
+				"ucsc.global", "parc.global"));
+		
+		groupListView = (ListView)findViewById(R.id.group_list);
+		groupAdapter = new ArrayAdapter<String>(this, 
+				android.R.layout.simple_list_item_multiple_choice, groupList);
+		groupAdapter.setNotifyOnChange(true);
+		
+		groupListView.setAdapter(groupAdapter);
+		groupListView.setItemsCanFocus(false);
+		groupListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+		groupListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				CheckedTextView ctv = (CheckedTextView)arg1;
+				System.out.println("hi!");
 			}
 		});
- 
 	}
- 
+	
+	private void initializeServiceList() {
+		serviceList = new ArrayList<String>(Arrays.asList(
+				"spencer", "printer"));
+		
+		serviceListView = (ListView)findViewById(R.id.service_list);
+		ArrayAdapter<String> serviceAdapter = new ArrayAdapter<String>(this, 
+				android.R.layout.simple_list_item_multiple_choice, serviceList);
+		serviceListView.setAdapter(serviceAdapter);
+		serviceListView.setItemsCanFocus(false);
+		serviceListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+		serviceListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				CheckedTextView ctv = (CheckedTextView)arg1;
+				System.out.println("hi!");
+			}
+		});
+	}
+	
+	// when "Refresh List" button clicked
+	public void refreshList(View view) {
+		groupList.add("test1.test2.global");
+		groupAdapter.notifyDataSetChanged();
+	}
+
+	// when "New AdHoc Group" button clicked
+	public void newGroup(View view) {
+		System.out.println("hi!");
+	}
 }
